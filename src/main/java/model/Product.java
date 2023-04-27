282110ec76d171fcda4eae675c96a74678ca5c6f
Steven Nguyen <steventn96@gmail.com>
2023-04-26 22:10:02 -0700

    started on Product.java class

---
+++ src/main/java/model/Product.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Product {
+    private Part part;
+    private int id;
+    private String name;
+    private double price;
+    private int stock;
+    private int min;
+    private int max;
+
+    public Product(Part part) {
+
+    }
+}
